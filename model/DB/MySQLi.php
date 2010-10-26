ed88355e9fcc566a79ece18fb8e9402de85504eb
bu <bu@hax4.in>
2010-10-26 15:49:09 +0800

    A new db adaptor for bPack, MySQLi (mysql driver for no pdo env)

---
+++ model/DB/MySQLi.php
@@ -0,0 +1,79 @@
+<?php
+    class bPack_DB_MySQLi
+    {
+        private $database_link;
+        public function __construct($config)
+        {
+            $this->database_link = mysql_connect($config->get(bPack_APP_ENV . '.host'), $config->get(bPack_APP_ENV . '.user'), $config->get(bPack_APP_ENV . '.password'));
+            
+            mysql_select_db($config->get(bPack_APP_ENV . '.name'));
+        }
+        
+        public function getEngine()
+        {
+            return $this;
+        }
+
+        public function query($sql)
+        {
+            return new bPack_DB_MySQLi_PDO_Statement($this->database_link, $sql);
+        }
+
+        public function exec($sql)
+        {
+            $rs = mysql_query($sql, $this->database_link);
+
+            if($rs === false)
+            {
+                return false;
+            }
+            else
+            {
+                return mysql_affected_rows();
+            }
+        }
+    }
+
+    class bPack_DB_MySQLi_PDO_Statement
+    {
+        public function __construct($link, $sql)
+        {
+            $this->rs = mysql_query($sql, $link);
+        }
+        
+        public function fetch($mode)
+        {
+            switch($mode)
+            {
+                case PDO::FETCH_ASSOC:
+                    return mysql_fetch_assoc($this->rs);
+                break;
+            }
+        }
+        
+        public function fetchAll($mode)
+        {
+            switch($mode)
+            {
+                case PDO::FETCH_ASSOC:
+                    $rows = array();
+
+                    while($row=mysql_fetch_assoc($this->rs))
+                    {
+                        $rows[] = $row;
+                    }
+                    return $rows;
+                break;
+            }
+        }
+
+        public function rowCount()
+        {
+            return mysql_num_rows($this->rs);
+        }
+
+        public function __destroy()
+        {
+            mysql_free_result($this->rs);
+        }
+    }

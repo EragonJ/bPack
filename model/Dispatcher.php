5fa2f7c6c782a9dbeec5d0f8594542dbdba4e219
bu <bu@hax4.in>
2010-12-03 16:45:31 +0800

    Request->all function were reassign to static function, refactor dispatcher.php, remove router

--- model/Dispatcher.php
+++ model/Dispatcher.php
@@ -1,36 +1,56 @@
 <?php
+/**
+ * bPack Dispatcher
+ */
+
 class bPack_Dispatcher
 {
     public static function run(bPack_DataContainer $route)
     {
-        $file_path = bPack_App_BaseDir . 'do/' . $route->module . '/' . $route->controller . '.php';
-
-        if(file_exists($file_path))
+        # check if the object(dispatcher) file exist
+        $file_path = self::checkObjectExists($route);
+        
+        # include the file
+        require_once $file_path;
+        
+        # initlazte the object
+        $className = self::generateClassname($route);
+        $object = new $className;
+        
+        # startup
+        call_user_func(array($object, 'startupAction'));
+        
+        # if parameter is given, try to give parameter
+        if(is_array($route->parameter) && isset($route->parameter))
         {
-            require_once($file_path);
-
-            $className = $route->module.'_'.$route->controller;
-
-            $obj = new $className;
-
-            call_user_func(array($obj, 'startupAction'));
-
-            if(is_array($route->parameter) && isset($route->parameter))
-            {
-                call_user_func_array(array($obj, $route->action), $route->parameter);
-            }
-            else
-            {
-                call_user_func(array($obj, $route->action));
-            }
-            
-            call_user_func(array($obj, 'tearDownAction'));
+            call_user_func_array(array($object, $route->action), $route->parameter);
         }
         else
+        {
+            call_user_func(array($object, $route->action));
+        }
+        
+        # teardown
+        call_user_func(array($object, 'tearDownAction'));
+
+        return true;
+    }
+
+    public static function checkObjectExists(bPack_DataContainer $route)
+    {
+        $file_path = bPack_Application_Directory . 'do/' . $route->module . '/' . $route->controller . '.php';
+
+        if(!file_exists($file_path))
         {
             throw new bPack_Exception('bPack Dispatcher: The request file is not accessible. (calling `'.$file_path.'`)');
         }
 
-        return true;
+        return $file_path;
+    }
+
+    public static function generateClassname($route)
+    {
+        return 'Controller_' . $route->module . '_' . $route->controller;
+
     }
 }
--- model/Request.php
+++ model/Request.php
@@ -8,32 +8,32 @@ define('bP_ARRAY',FILTER_REQUIRE_ARRAY);
 
 class bPack_Request
 {
-    public function get($var_name ,$default_value = false, $type = bP_STRING,$option = '')
+    public static function get($var_name ,$default_value = false, $type = bP_STRING,$option = '')
     {
-        return $this->_input(INPUT_GET,$var_name ,$default_value, $type,$option);
+        return self::_input(INPUT_GET,$var_name ,$default_value, $type,$option);
     }
 
-    public function post($var_name ,$default_value = false, $type = bP_STRING,$option = '')
+    public static function post($var_name ,$default_value = false, $type = bP_STRING,$option = '')
     {
-        return $this->_input(INPUT_POST,$var_name ,$default_value, $type,$option);
+        return self::_input(INPUT_POST,$var_name ,$default_value, $type,$option);
     }
 
-    public function cookie($var_name ,$default_value = false, $type = bP_STRING,$option = '')
+    public static function cookie($var_name ,$default_value = false, $type = bP_STRING,$option = '')
     {
-        return $this->_input(INPUT_COOKIE,$var_name ,$default_value, $type,$option);
+        return self::_input(INPUT_COOKIE,$var_name ,$default_value, $type,$option);
     }
 
-    public function server($var_name ,$default_value = false, $type = bP_STRING,$option = '')
+    public static function server($var_name ,$default_value = false, $type = bP_STRING,$option = '')
     {
-        return $this->_input(INPUT_SERVER,$var_name ,$default_value, $type,$option);
+        return self::_input(INPUT_SERVER,$var_name ,$default_value, $type,$option);
     }
 
-    public function clean($value, $type = bP_STRING)
+    public static function clean($value, $type = bP_STRING)
     {
         return filter_var($value, $type);
     }
 
-    protected function _input($var_type ,$var_name,$default_value = false, $type = bP_STRING, $option = '')
+    protected static function _input($var_type ,$var_name,$default_value = false, $type = bP_STRING, $option = '')
     {
         $value = filter_input($var_type,$var_name,$type,$option);
         
@@ -47,7 +47,7 @@ class bPack_Request
         }
     }
     
-    public function load($var, $default_value = '')
+    public static function load($var, $default_value = '')
     {
         if(isset($var))
         {
@@ -58,4 +58,6 @@ class bPack_Request
             return $default_value;
         }
     }
+
+    
 }

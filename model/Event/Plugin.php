dce179b1c1e709ebf2106fc16732b3ab51e0c7be
bu <bu@hax4.in>
2010-12-07 11:44:41 +0800

    Refactor Plugin Structure, using template pattern

--- model/Event/Plugin.php
+++ model/Event/Plugin.php
@@ -2,5 +2,18 @@
 
 abstract class bPack_Event_Plugin
 {
+    protected $parent = null;
 
+    final public function __construct(bPack_Event_Model $parent)
+    {
+        $this->parent = $parent;
+
+        // setup plugin dependency
+        $this->pluginInitialization();
+        // register plugin functions to the Controller
+        $this->registerFunctions();
+    }
+
+    abstract protected function pluginInitialization();
+    abstract protected function registerFunctions();
 }

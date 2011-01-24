f9edeb2456bdf8fe22015d0e5d3f4eb2dc4d0c3b
bu <bu@hax4.in>
2011-01-25 02:26:06 +0800

    add comment for easier understanding

--- model/DataContainer.php
+++ model/DataContainer.php
@@ -1,13 +1,42 @@
 <?php
+/**
+ * Creating object that stores key-value data 
+ * for passing though bPack Modules
+ * 
+ * @author bu <bu@hax4.in>
+ * @package bPack
+ * @subpackage DataContainer
+ */
+
 class bPack_DataContainer
 {
+    /**
+     * The stored data
+     *
+     * @access protected
+     * @var array
+     */
     protected $data = array();
 
+    /**
+     * Setter method
+     * 
+     * @param string $name the key of the value
+     * @param mixed $value the value being stored
+     */
     public function __set($name,$value)
     {
         $this->data[$name] = $value;
     }
 
+    /**
+     * Getter method
+     *
+     * return false if the key haven't been set; otherwise, return value
+     * 
+     * @param string $name the key-name of the request value
+     * @return mixed
+     */
     public function __get($name)
     {
         if(isset($this->data[$name]))
@@ -19,7 +48,13 @@ class bPack_DataContainer
             return false;
         }
     }
-
+    
+    /**
+     * Dump the stored data
+     *
+     * @param void
+     * @return array
+     */
     public function getStoredData()
     {
         return $this->data;

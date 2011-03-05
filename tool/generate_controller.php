9f64fce27ca984b4fe8211e10d53665523cb2cc9
bu <bu@hax4.in>
2011-03-05 20:34:14 +0800

    a tool helps creating controller

---
+++ tool/generate_controller.php
@@ -0,0 +1,53 @@
+<?php
+
+$options = $argv;
+
+$actions = array(
+    'defaultAction',
+    'startupAction',
+    'tearDownAction'
+);
+
+if(isset($argv[1])) {
+    $module = $argv[1]; 
+}
+else
+{
+    $module = 'default';
+}
+
+if(isset($argv[2])) {
+    $controller = $argv[2];
+}
+else
+{
+    $controller = 'default';
+}
+
+for($i=3;$i<=(sizeof($argv));$i++) {
+    if(isset($argv[$i])) {
+        $actions[] = $argv[$i];
+    }
+}
+
+$ApplicationController = 'ApplicationController';
+
+$controller_file = "<?php
+
+class Controller_{$module}_{$controller} extends {$ApplicationController} {\n\n";
+
+foreach($actions as $action_method) {
+    $controller_file .= "    public function {$action_method}() {\n        #TODO: {$action_method}\n    }\n\n";
+}
+
+$controller_file .= "}";
+
+if(!file_exists('do/'.$module)) {
+    mkdir('do/'.$module);
+}
+
+file_put_contents('do/'.$module.'/'.$controller.'.php', $controller_file);
+
+echo "\n\n";
+echo 'Successfully Created!!';
+echo "\n\n";

8d225fe381caaefa93b88debb4a9daa1a57f3040
bu <bu@hax4.in>
2011-12-24 15:56:54 +0800

    a rewrite of this module, now supported XHR and form input

--- model/Upload.php
+++ model/Upload.php
@@ -2,90 +2,161 @@
 
 class bPack_Upload
 {
-	protected $field_name;
-	protected $new_filename;
-	protected $new_save_path;
-	
-	public function setFieldName($field_name = NULL)
-	{			
-		$this->field_name = $field_name;
-		$this->new_filename = '';
-		$this->new_save_path = './';
-		
+	private $_field_name;
+
+	protected $_handlers = array();
+	protected $_current_handler = null;
+
+	protected $_file_name = 'untitled';
+	protected $_file_ext = 'tmp';
+
+	public function __construct($field_name)
+	{
+		$this->_setField($field_name);
+
+		$this->addHandler(new bPack_Upload_XHR);
+		$this->addHandler(new bPack_Upload_Form);
+	}
+
+	public function addHandler(bPack_Upload_Handler $handle_obj)
+	{
+		$this->_handlers[] = $handle_obj;
+
+		$handle_obj->setFieldName($this->_field_name);
+	}
+
+	protected function _setField($field_name = NULL)
+	{
+		if(is_null($field_name)) 
+		{
+			throw new bPack_Exception("Field name not given.");
+		}
+
+		$this->_field_name = $field_name;
+	}
+
+	public function checkUpload()
+	{
+		foreach($this->_handlers as $handler)
+		{
+			if($handler->match())
+			{
+				$this->_current_handler = &$handler;
+
+				return true;
+			}
+		}
+
+		throw new bPack_Exception('No handler fit request');
+		return false;
+	}
+
+	public function setFilename($filename)
+	{
+		$this->_file_name = $filename;
+
 		return $this;
 	}
-	
-	public function isUploaded()
-	{			
-		if( (isset($_FILES[$this->field_name])) && ($_FILES[$this->field_name]['tmp_name']!="") ) 
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	public function setName($new_filename = NULL)
+
+	public function setExt($ext)
 	{
-		if($new_filename == NULL)
+		$this->_file_ext = $ext;
+
+		return $this;
+	}
+
+	public function getFilename($with_ext = true)
+	{
+		if($with_ext)
 		{
-			$new_filename = md5(microtime(true));
+			return $this->_file_name . '.' . $this->_file_ext;
 		}
-		
-		$this->new_filename = $new_filename;
-		
+
+		return $this->_file_name;
+	}
+
+	public function saveTo($save_path = './')
+	{
+		 return $this->_current_handler->moveTo($save_path, $this->getFilename());
+	}
+}
+
+abstract class bPack_Upload_Handler
+{
+	public function setFieldName($field_name)
+	{
+		$this->_field_name = $field_name;
+
 		return true;
 	}
-	
-	public function getExt()
+
+	abstract public function match();
+	abstract public function moveTo($save_path, $filename);
+	abstract public function getSize();
+}
+
+class bPack_Upload_XHR extends bPack_Upload_Handler
+{
+	public function match()
 	{
-		return strtolower(substr($_FILES[$this->field_name]['name'], strrpos($_FILES[$this->field_name]['name'] , ".")+1));
-	}		
-	
-	public function getName()
+		$xhr_header = bPack_Request::server('HTTP-X-Requested-With', NULL); 
+
+		return (!is_null($xhr_header));
+	}
+
+	public function getSize()
 	{
-		if(empty($this->new_filename))
+		return bPack_Request::server('Content-Length', 0);
+	}
+
+	public function moveTo($save_path, $filename)
+	{
+		$input = fopen("php://input", "r");
+        $temp = tmpfile();
+
+        $realSize = stream_copy_to_stream($input, $temp);
+        fclose($input);
+        
+        if ( $realSize != $this->getSize() )
 		{
-			$this->new_filename = md5(microtime(true));
+            return false;
+        }
+        
+        $target = fopen($save_path . $filename, "w");        
+        fseek($temp, 0, SEEK_SET);
+
+        stream_copy_to_stream($temp, $target);
+        fclose($target);
+        
+        return true;
+	}
+}
+
+class bPack_Upload_Form extends bPack_Upload_Handler
+{
+	public function match()
+	{
+		return ( (isset($_FILES[$this->_field_name])) && ($_FILES[$this->_field_name]['tmp_name']!="") );
+	}
+
+	public function moveTo($save_path, $filename)
+	{
+		if(is_uploaded_file($this->_getTempFilePath()))
+		{
+			return move_uploaded_file($this->_getTempFilePath(), $save_path . $filename);
 		}
-		
-		return $this->new_filename;
+
+		throw new bPack_Exception("Request file is not uploaded by PHP");
+
 	}
-	
-	public function setSavePath($path = './')
+
+	protected function _getTempFilePath()
 	{
-		$this->new_save_path = $path;
-		
-		return true;
-	}				
-	
-	public function getSavePath()
-	{			
-		return $this->new_save_path;
-	}		
-	
-	public function save()
-	{			
-		$save_filename = $this->getName() . '.' . $this->getExt();
-		
-		$flag = copy($_FILES[$this->field_name]['tmp_name'], $this->new_save_path. $save_filename);
-		
-		return $flag;
+		return $this->_FILES[$this->_field_name]['tmp_name'];
 	}
-	
-	public function getFullPath()
+
+	public function getSize()
 	{
-		$save_filename = $this->getName() . '.' . $this->getExt();
-		
-		return $this->new_save_path. $save_filename;
-	}		
-	
-	public function getFullName()
-	{
-		$save_filename = $this->getName() . '.' . $this->getExt();
-		
-		return $save_filename;
+		return $this->_FILES[$this->_field_name]['size'];
 	}
 }
